761c67d9adee7d7f6492c29159f8267333bc6d99
yanghanwei <yanghanwei@geotmt.com>
2019-10-31 15:41:54 +0800

    AOP

--- src/main/java/com/hwy/study01/aop/LogAnnotation.java
+++ src/main/java/com/hwy/study01/aop/LogAnnotation.java
@@ -9,6 +9,23 @@ import java.lang.annotation.*;
  * @Mail        yanghanwei@geotmt.com
  * @Date        12:15 2019-10-31
  * @Version     v1
+ *
+ * @Target注解，是专门用来限定某个自定义注解能够被应用在哪些Java元素上面的
+    类，接口（包括注解类型）或枚举的声明 TYPE,
+    属性的声明  FIELD,
+    方法的声明  METHOD,
+    方法形式参数声明  PARAMETER,
+    构造方法的声明  CONSTRUCTOR,
+    局部变量声明  LOCAL_VARIABLE,
+    注解类型声明  ANNOTATION_TYPE,
+    包的声明  PACKAGE
+
+   @Retention注解，翻译为持久力、保持力。即用来修饰自定义注解的生命力
+    SOURCE  注解将被编译器忽略掉
+    CLASS   注解将被编译器记录在class文件中，但在运行时不会被虚拟机保留，这是一个默认的行为
+    RUNTIME 注解将被编译器记录在class文件中，而且在运行时会被虚拟机保留，因此它们能通过反射被读取到
+   @Documented注解，是被用来指定自定义注解是否能随着被定义的java文件生成到JavaDoc文档当中。
+   @Inherited注解，是指定某个自定义注解如果写在了父类的声明部分，那么子类的声明部分也能自动拥有该注解。@Inherited注解只对那些@Target被定义为ElementType.TYPE的自定义注解起作用
  **/
 @Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/com/hwy/study01/aop/LogAspect.java
+++ src/main/java/com/hwy/study01/aop/LogAspect.java
@@ -19,6 +19,15 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 public class LogAspect {
 
+    /**
+     * 例： execution (* com.sample.service..*. *(..))
+     * 整个表达式可以分为五个部分：
+     * 1、execution():：表达式主体。
+     * 2、第一个*号：表示返回类型， *号表示所有的类型。
+     * 3、包名：表示需要拦截的包名，后面的两个句点表示当前包和当前包的所有子包，com.sample.service包、子孙包下所有类的方法。
+     * 4、第二个*号：表示类名，*号表示所有的类。
+     * 5、*(..)：最后这个星号表示方法名，*号表示所有的方法，后面括弧里面表示方法的参数，两个句点表示任何参数
+     */
     @Pointcut("execution(* com.hwy.study01.controller.OrderController.*(..))")
     public void pcut1(){}
 
@@ -38,6 +47,11 @@ public class LogAspect {
     }
 
 
+    /**
+     * 切点为 注解的方式
+     * @param joinPoint
+     * @param logAnnotation
+     */
     @Before("@annotation(logAnnotation)")
     public void afterPrintLog(JoinPoint joinPoint, LogAnnotation logAnnotation){
         System.out.println("================ after =================");
@@ -46,6 +60,10 @@ public class LogAspect {
         System.out.println("================ after =================");
     }
 
+    /**
+     * 使用多个切点
+     * @param point
+     */
     @Before("pcut1() || pcut2()")
     public void beforeController(JoinPoint point) {
         System.out.println("================ pcut after =================");
